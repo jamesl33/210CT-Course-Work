dc9b17015fa6bb9a80f7d76fe21005a1a2ebb7f7
James Lee <jamesl33info@gmail.com>
2017-10-14 00:11:48 +0100

    added task1 in c++ with unit testing and pseudo code

---
+++ C++/task1/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#define CATCH_CONFIG_MAIN
+#include "catch.h"
+
+long factorial(long n)
+{
+	if(n == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return n * factorial(n - 1);
+	}
+}
+
+bool test_if_divides(int a, int b)
+{
+	if(factorial(a) % b)
+	{
+		return false;
+	}
+	return true;
+}
+
+// ################# Pseudo Code #################
+
+// FACTORIAL(n)
+// 	IF n = 0
+// 		RETURN 1
+// 	ELSE
+// 		RETURN n * FACTORIAL(n - 1)
+
+// TEST-IF-DEVIDES(a, b)
+// 	IF a! MOD b = 0
+// 		RETURN true
+// 	ELSE
+// 		RETURN false
+
+// ################# Pseudo Code #################
+
+// ################# Unit Test #################
+
+TEST_CASE("Factorials are computed", "[factorial]")
+{
+    REQUIRE(factorial(1) == 1);
+    REQUIRE(factorial(2) == 2);
+    REQUIRE(factorial(3) == 6);
+    REQUIRE(factorial(10) == 3628800);
+}
+
+TEST_CASE("Test if factorial(a) % b == 0", "[test_if_divides]")
+{
+	REQUIRE(test_if_divides(6, 9) == 1);
+	REQUIRE(test_if_divides(20, 10000) == 1);
+	REQUIRE(test_if_divides(6, 27) == 0);
+	REQUIRE(test_if_divides(20, 1000000) == 0);
+}
+
+// ################# Unit Test #################
