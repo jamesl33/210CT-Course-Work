4bd01e288994021cd85aeda697b7b70aab02ffe4
James Lee <jamesl33info@gmail.com>
2017-10-14 23:26:45 +0100

    Added docstrings

--- C++/task1/main.cpp
+++ C++/task1/main.cpp
@@ -3,6 +3,12 @@
 #include "catch.h"
 
 long factorial(long n) {
+	/*
+	input:
+		long n: number which you want the factorial of
+	output:
+		long: returns n!
+	*/
 	if (n == 0) {
 		return 1;
 	} else {
@@ -11,6 +17,13 @@ long factorial(long n) {
 }
 
 bool test_if_divides(int a, int b) {
+	/*
+	input:
+		int a: number we calculate the factorial of and then is tested if divides by 'b' equally
+		int b: a!' is divided by this number
+	output:
+		bool: boolean representing whether '(a! / b) == 0'
+	*/
 	if (factorial(a) % b) {
 		std::cout << b << " does not divide by " << a << "!" << std::endl;
 		return false;
