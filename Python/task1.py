f35473170a90f3e177f703682238221038c134ca
James Lee <jamesl33info@gmail.com>
2017-10-14 23:19:39 +0100

    Changed docstrings to contain class information. Updated unit testing.

--- Python/task1.py
+++ Python/task1.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
 def factorial(n):
-	""" returns factorial of input "n" alternative to using math.factorial"""
+	"""
+	alternative to using math.factorial()
+	arguments:
+		integer 'n': number which you want the factorial of
+	output:
+		integer 'n!': returns the factorial of 'n' 
+	"""
 	if type(n) != int:
 		raise TypeError('n: needs to be of type integer')
 
@@ -11,14 +17,23 @@ def factorial(n):
 		return n * factorial(n - 1)
 
 def testIfDivides(a, b):
+	"""
+	arguments:
+		integer 'a': number we calculate the factorial of and then is tested if divides by 'b' equally
+		integer 'b: 'a!' is divided by this number
+	output:
+		touple[0]:
+			string: string which is will be printed e.g '9 divides by 6!'
+		touple[1]:
+			bool: boolean representing if 'a! / b' is equal to 0
+
+	"""
 	if type(a) != int or type(b) != int:
 		raise TypeError('testIfDivides only takes integers as arguments')
 		
 	if factorial(a) % b:
-		print("\n{0} does not divide by {1}!".format(b, a))
-		return False
-	print("\n{0} divides by {1}!".format(b, a))
-	return True
+		return("{0} does not divide by {1}!".format(b, a), False)
+	return("{0} divides by {1}!".format(b, a), True)
 
 ################# Pseudo Code #################
 
@@ -36,22 +51,34 @@ def testIfDivides(a, b):
 
 ################# Pseudo Code #################
 
+################# Labsheet Test #################
+
+# Test values from the labsheet
+testValues = [(6, 9), (20, 10000), (6, 27), (20, 1000000)]
+print()
+for a, b in testValues:
+	print(testIfDivides(a, b)[0])
+print()
+
+################# Labsheet Test #################
+
 ################# Unit Test #################
 
+# Unit Test
 import unittest
 
 class UnitTest(unittest.TestCase):
 	def test_correct(self):
 		known_correct_values = [(6, 9), (20, 10000)]
 		for a, b in known_correct_values:
-			self.assertTrue(testIfDivides(a, b))
+			self.assertTrue(testIfDivides(a, b)[1])
 
 	def test_false(self):
-		known_wrong_values = [(6,27), (20, 1000000)]
+		known_wrong_values = [(6, 27), (20, 1000000)]
 		for a, b in known_wrong_values:
-			self.assertFalse(testIfDivides(a,b))
+			self.assertFalse(testIfDivides(a,b)[1])
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
 
 ################# Unit Test #################
--- Python/task2.py
+++ Python/task2.py
@@ -2,7 +2,12 @@
 
 class material:
 	def __init__(self, name, quantity, price_per_kilo):
-		""" instantiate settings base values """
+		"""
+		class attributes:
+			__name: string representing the name of the material
+			__pricePerKilo: integer representing the price of the material per kilogram
+			__quantity: integer for the total amount of the material which is available
+		"""
 		if type(quantity) != int:
 			raise TypeError('quantity: must be of type int')
 
@@ -13,11 +18,11 @@ class material:
 			raise TypeError ('price_per_kilo: must be of type integer')
 
 		self.__name = name
-		self.__price_per_kilo = price_per_kilo
+		self.__pricePerKilo = price_per_kilo
 		self.__quantity = quantity
 
 	def get_price(self):
-		return self.__price_per_kilo
+		return self.__pricePerKilo
 
 	def get_quantity(self):
 		return self.__quantity
@@ -29,36 +34,61 @@ class material:
 		self.__quantity -= 1
 
 	def __str__(self):
-		""" displays 'name', 'price per kilogram' and 'quantity' followed by a new line """
+		"""
+		output: 
+			displays 'name', 'price per kilogram' and 'quantity' followed by a new line 
+		"""
 		info = ''
-		info = info + 'Name: {0}\nPPK: {1}\nQuantity: {2}\n'.format(self.__name, self.__price_per_kilo, self.__quantity)
+		info = info + 'Name: {0}\nPPK: {1}\nQuantity: {2}\n'.format(self.get_name(), self.get_price(), self.get_quantity())
 		return info
 
 class lorry:
-	def __init__(self, max_load):
-		""" instantiate settings base values """
-		if type(max_load) != int:
-			raise TypeError('max_load: must be of type integer')
+	def __init__(self, maxLoad):
+		"""
+		class attributes:
+			__maxLoad: integer representing the maximum load the lorry can hold
+			__loadComposition: interger representing the price of the cargo in the lorry
+			__cargo: dictionary representing the cargo held in the lorry
+		"""
+		if type(maxLoad) != int:
+			raise TypeError('maxLoad: must be of type integer')
 
-		self.__max_load = max_load
-		self.__load_composition = 0
+		self.__maxLoad = maxLoad
+		self.__loadComposition = 0
 		self.__cargo = {} # cargo is a dictionary {material name: amount}
 
 	def get_cargo(self):
 		return self.__cargo
 
-	def get_composition(self):
-		return self.__load_composition
+	def get_loadComposition(self):
+		return self.__loadComposition
+
+	def get_maxLoad(self):
+		return self.__maxLoad
+
+	def set_cargo(self, cargo):
+		self.__cargo = cargo
+
+	def set_loadComposition(self, loadComposition):
+		self.__loadComposition = loadComposition
 
 	def current_cargo_weight(self):
-		""" return current weight of cargo in lorry; uses 'self.__cargo' and adds up amounts of each material into a total value """
+		"""
+		output:
+			returns the current cargo weight in the lorry
+		"""
 		weight = 0
 		for material in self.__cargo:
 			weight += self.__cargo[material]
 		return weight
 
 	def most_expensive_material(self, materials):
-		""" finds the most expensive item in list of "material" objects """
+		"""
+		arguments:
+			list: list of 'material' objects
+		output:
+			integer: index of the most expensive 'material' object in the 'materials' list
+		"""
 		cost = 0
 		for i in range(len(materials)):
 			if (materials[i].get_price() > cost) and (materials[i].get_quantity() > 0):
@@ -67,29 +97,61 @@ class lorry:
 		return most_expensive
 
 	def pickup_deliviery(self, materials):
-		""" 'pickup' delivery; choose the most expensive load that the lorry can load """
+		"""
+		arguments:
+			list: list of 'material' objects
+		output:
+			NONE: there is no output however 'self.__cargo' is updated with the most expensive load possible using the input list
+		"""
 		if type(materials) != list:
 			raise TypeError('materials: should be a list of materials')
 
-		while self.current_cargo_weight() < self.__max_load:
+		while self.current_cargo_weight() < self.get_maxLoad():
+			cargo = self.get_cargo()
+			loadComposition = self.get_loadComposition()
 			most_expensive_index = self.most_expensive_material(materials)
-			if materials[most_expensive_index].get_name() in self.__cargo:
-				self.__cargo[(materials[most_expensive_index].get_name())] += 1
-				self.__load_composition += materials[most_expensive_index].get_price()
-			else:
-				self.__cargo[(materials[most_expensive_index].get_name())] = 1
-				self.__load_composition += materials[most_expensive_index].get_price()
 
+			if materials[most_expensive_index].get_name() in cargo:
+				cargo[(materials[most_expensive_index].get_name())] += 1
+				loadComposition += materials[most_expensive_index].get_price()
+			else:
+				cargo[(materials[most_expensive_index].get_name())] = 1
+				loadComposition += materials[most_expensive_index].get_price()
+
+			self.set_cargo(cargo)
+			self.set_loadComposition(loadComposition)
 			materials[most_expensive_index].decriment()
 
 	def __str__(self):
-		""" dislays load composition and cargo when printing lorry object """
-		info = 'Load composition value = {0}\n'.format(self.__load_composition)
-		for key in self.__cargo:
-			info = info + '{0}kg of {1} and '.format(self.__cargo[key], key)
+		"""
+		output:
+			returns a string containing info about current load and its value
+		"""
+		info = 'Load composition value = {0}\n'.format(self.get_loadComposition())
+		for key in self.get_cargo():
+			info = info + '{0}kg of {1} and '.format(self.get_cargo()[key], key)
 		info = info[:-4]
 		info = info +'\n'
 		return info
+
+################# Pseudo Code #################
+################# Pseudo Code #################
+
+################# Labsheet Test #################
+
+print()
+known_correct_values = {"Gold": 4, "Copper": 6}
+
+gold = material('Gold', 4, 100)
+copper = material('Copper', 7, 65)
+plastic = material('Plastic', 15, 50)
+
+materials = [gold, plastic, copper]
+lorry1 = lorry(10)
+lorry1.pickup_deliviery(materials)
+print(lorry1)
+
+################# Labsheet Test #################
 
 ################# Unit Test #################
 
@@ -108,7 +170,7 @@ class UnitTest(unittest.TestCase):
 		lorry1.pickup_deliviery(materials)
 
 		self.assertEqual(lorry1.get_cargo(), known_correct_values)
-		self.assertEqual(lorry1.get_composition(), 790)
+		self.assertEqual(lorry1.get_loadComposition(), 790)
 
 	def test_extra(self):
 		known_correct_values = {"Ruby": 2, "Copper": 7, "Diamond": 1, "Plastic": 1, "Gold": 4}
@@ -125,7 +187,7 @@ class UnitTest(unittest.TestCase):
 		lorry1.pickup_deliviery(materials)
 
 		self.assertEqual(lorry1.get_cargo(), known_correct_values)
-		self.assertEqual(lorry1.get_composition(), 2905)
+		self.assertEqual(lorry1.get_loadComposition(), 2905)
 
 if __name__ == '__main__':
     unittest.main()
