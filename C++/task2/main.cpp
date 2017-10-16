bd8db4b3893fb27d81441b94f18398c60d7d92c1
James Lee <jamesl33info@gmail.com>
2017-10-16 13:15:17 +0100

    Reformatted code. Added overload function for '<<'

--- C++/task2/main.cpp
+++ C++/task2/main.cpp
@@ -1,39 +1,36 @@
-#define CATCH_CONFIG_MAIN
 #include <iostream>
 #include <string>
 #include <vector>
-#include "catch.h"
 #include "material.h"
 #include "lorry.h"
 
-// ################# Unit Test #################
+// ################# Labsheet main ################# // Uncomment code inside of 'Labsheet main' when not running unit testing
 
-TEST_CASE("Most expensive load is calculated", "[lorry]")
-{
-	material gold=material();
-	gold.set_name("Gold");
-	gold.set_quantity(4);
-	gold.set_price_per_kilo(100);
+// int main() {
+// 	material gold=material("Gold", 4, 100);
+// 	material copper=material("Copper", 7, 65);
+// 	material plastic=material("Plastic", 15, 50);
+// 	std::vector<material> materials_labsheet = {gold, plastic, copper};
 
-	material copper=material();
-	copper.set_name("Copper");
-	copper.set_quantity(7);
-	copper.set_price_per_kilo(65);
+// 	lorry lorry1=lorry(10);
+// 	lorry1.pickup_delivery(materials_labsheet);
+// 	std::cout << lorry1 << std::endl;
+// } 
 
-	material plastic=material();
-	plastic.set_name("Plastic");
-	plastic.set_quantity(15);
-	plastic.set_price_per_kilo(50);
+// ################# Labsheet main #################
 
-	material diamond=material();
-	diamond.set_name("Diamond");
-	diamond.set_quantity(1);
-	diamond.set_price_per_kilo(1000);
+// ################# Unit Test ################# // Comment out code inside of 'Unit Test' when not unit testing
 
-	material ruby=material();
-	ruby.set_name("Ruby");
-	ruby.set_quantity(2);
-	ruby.set_price_per_kilo(500);
+#define CATCH_CONFIG_MAIN
+#include "catch.h"
+
+TEST_CASE("Most expensive load is calculated", "[lorry]") {
+	// create all the material objects
+	material gold=material("Gold", 4, 100);
+	material copper=material("Copper", 7, 65);
+	material plastic=material("Plastic", 15, 50);
+	material diamond=material("Diamond", 1, 1000);
+	material ruby=material("Ruby", 2, 500);
 
 	std::vector<material> materials_labsheet = {gold, plastic, copper};
 	std::vector<material> materials_extra = {gold, plastic, copper, diamond, ruby};
@@ -41,18 +38,16 @@ TEST_CASE("Most expensive load is calculated", "[lorry]")
 	std::map<std::string, int> know_values_labsheet = {{"Gold", 4}, {"Copper", 6}};
 	std::map<std::string, int> know_values_extra = {{"Diamond", 1}, {"Ruby", 2}, {"Gold", 4}, {"Copper", 7}, {"Plastic", 1}};
 
-	lorry lorry1=lorry();
-	lorry1.set_max_load(10);
+	lorry lorry1=lorry(10);
 	lorry1.pickup_delivery(materials_labsheet);
+	std::cout << lorry1 << std::endl << std::endl;
+
+	lorry lorry2=lorry(15);
+	lorry2.pickup_delivery(materials_extra);
+	std::cout << lorry2 << std::endl << std::endl;
 
 	REQUIRE(lorry1.get_cargo() == know_values_labsheet);
-
-	lorry lorry2=lorry();
-	lorry2.set_max_load(15);
-	lorry2.pickup_delivery(materials_extra);
-
 	REQUIRE(lorry2.get_cargo() == know_values_extra);
-
 }
 
 // ################# Unit Test #################
--- C++/task2/material.h
+++ C++/task2/material.h
@@ -1,17 +1,21 @@
 #include <string>
 
-class material
-{
+class material {
 	public:
-		// setters
+		material(std::string nme, int qty, int price_per_kilo) {
+			name = nme;
+			quantity = qty;
+			ppk = price_per_kilo;
+		}
+
 		void set_name(std::string nme);
 		void set_quantity(int qty);
 		void set_price_per_kilo(int price);
-		// getters
+
 		std::string get_name();
 		int get_quantity();
 		int get_price_per_kilo();
-		// functions
+
 		void decriment();
 	
 	private:
@@ -20,37 +24,30 @@ class material
 		int ppk;
 };
 
-void material::set_name(std::string nme) 
-{
+void material::set_name(std::string nme) {
 	name = nme;
 }
 
-void material::set_quantity(int qty) 
-{
+void material::set_quantity(int qty) {
 	quantity = qty;
 }
 
-void material::set_price_per_kilo(int price)
-{
+void material::set_price_per_kilo(int price) {
 	ppk = price;
 }
 
-std::string material::get_name()
-{
+std::string material::get_name() {
 	return name;
 }
 
-int material::get_quantity() 
-{
+int material::get_quantity() {
 	return quantity;
 }
 
-int material::get_price_per_kilo() 
-{
+int material::get_price_per_kilo() {
 	return ppk;
 }
 
-void material::decriment() 
-{
+void material::decriment() {
 	quantity--;
 }
