1128ee0107a313f86d28d6075a462b9114466243
James Lee <jamesl33info@gmail.com>
2017-10-18 15:57:01 +0100

    updated to include private attributes

--- C++/task3/main.cpp
+++ C++/task3/main.cpp
@@ -3,23 +3,29 @@
 #include <algorithm>
 #include "queen.hpp"
 
-// ################# Labsheet main ################# // Uncomment code inside of 'Labsheet main' when not running unit testing
+// ################# Labsheet main ################# // Comment out code inside of 'Labsheet Main' when running unit test
 
 int main() {
-	queen solver = queen(8);
-	solver.print_vector(solver.place_queen());
+	for (int i = 0; i < 8; i++) {
+		queen solver = queen(8);
+		std::cout << "Solution " << i + 1 << " - ";
+		solver.print_vector(solver.place_queen(i));
+	}
 }
 
 // ################# Labsheet main #################
 
-// ################# Unit Test ################# // Comment out code inside of 'Unit Test' when not unit testing
+// ################# Unit Test ################# // Uncomment code inside of 'Unit Test' when unit testing
 
 // #define CATCH_CONFIG_MAIN
 // #include "catch.hpp"
 
 // TEST_CASE("Check is its safe to place a queen", "[is_safe]") {
 // 	queen solver = queen(8);
-// 	solver.boardState.emplace_back(0);
+// 	std::vector<int> v = solver.get_board_state();
+// 	v.emplace_back(0);
+// 	solver.set_board_state(v);
+
 // 	REQUIRE((solver.is_safe(0, 1)) == false);
 // 	REQUIRE((solver.is_safe(1, 0)) == false);
 // 	REQUIRE((solver.is_safe(1, 1)) == false);
@@ -28,9 +34,17 @@ int main() {
 // }
 
 // TEST_CASE("Check if place_queen returns correct solution to 8 queens", "[place_queen]") {
-// 	queen solver = queen(8);
-// 	std::vector<int> knowCorrectValues = {0, 4, 7, 5, 2, 6, 1, 3};
-// 	REQUIRE(solver.place_queen() == knowCorrectValues);
+// 	std::vector<std::vector<int>> knownValues;
+// 	knownValues = {{0, 4, 7, 5, 2, 6, 1, 3}, {1, 3, 5, 7, 2, 0, 6, 4},
+// 				   {2, 0, 6, 4, 7, 1, 3, 5}, {3, 0, 4, 7, 1, 6, 2, 5},
+// 				   {4, 0, 3, 5, 7, 1, 6, 2}, {5, 0, 4, 1, 7, 2, 6, 3},
+// 				   {6, 0, 2, 7, 5, 3, 1, 4}, {7, 1, 3, 0, 6, 4, 2, 5}};
+
+// 	for (int i = 0; i < 8; i++) {
+// 		queen solver = queen(8);
+// 		std::vector<int> proposedSolution = solver.place_queen(i);
+// 		REQUIRE(std::find(knownValues.begin(), knownValues.end(), proposedSolution) != knownValues.end());
+// 	}
 // }
 
 // ################# Unit Test #################
--- C++/task3/queen.hpp
+++ C++/task3/queen.hpp
@@ -8,17 +8,40 @@ class queen {
 			boardSize = bs;
 		}
 
-		int boardSize;
-		std::vector<int> boardState;
+		int get_board_size();
+		std::vector<int> get_board_state();
+
+		void set_board_size(int bs);
+		void set_board_state(std::vector<int> bs);
 
 		bool is_safe(int x, int y);
 		std::vector<int> place_queen(int pos);
 		void print_vector(std::vector<int> board);
+
+	private:
+		int boardSize;
+		std::vector<int> boardState;
 };
+
+int queen::get_board_size() {
+	return boardSize;
+}
+
+std::vector<int> queen::get_board_state() {
+	return boardState;
+}
+
+void queen::set_board_size(int bs) {
+	boardSize = bs;
+}
+
+void queen::set_board_state(std::vector<int> bs) {
+	boardState = bs;
+}
 
 bool queen::is_safe(int x, int y) {
 	for (int col = 0; col < boardState.size(); col++) {
-		int row = boardState[col];
+		int row = get_board_state()[col];
 		if (x == col || y == row) {
 			return false;
 		}
@@ -30,24 +53,27 @@ bool queen::is_safe(int x, int y) {
 }
 
 std::vector<int> queen::place_queen(int pos=0) {
+	std::vector<int> boardState = get_board_state();
 	if (boardState.size() == boardSize) {
 		return boardState;
 	}
 	for (int col = pos; col < boardSize; col++) {
 		if (is_safe(boardState.size(), col)) {
 			boardState.emplace_back(col);
+			set_board_state(boardState);
 			return place_queen();
 		}
 	}
 	int lastQueenRow = boardState.back();
 	boardState.pop_back();
+	set_board_state(boardState);
 	return place_queen(lastQueenRow + 1);
 }
 
 void queen::print_vector(std::vector<int> board) {
 	for (auto i = board.begin(); i != board.end(); i++) {
 		if (i == board.begin()) {
-			std::cout << "[";
+			std::cout << "[" << *i << ", ";
 		} else if (next(i) == board.end()) {
 			std::cout << *i << "]\n";
 		} else {
