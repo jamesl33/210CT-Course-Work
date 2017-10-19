1936152da20cfe91efe0f9d84228bcf81fdcd2d9
James Lee <jamesl33info@gmail.com>
2017-10-19 14:06:15 +0100

    started task4

---
+++ Python/task4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+class cube:
+    def __init__(self, edgeLength, color):
+        self.__edgeLength = edgeLength
+        self.__color = color
+
+
+################# Pseudo Code #################
+
+cubeList = []
+stack_cubes(cubeList)
+    WHILE
+
+################# Pseudo Code #################
+
+################# Labsheet Main ################# Comment out code inside of 'Labsheet Main' when running unit test
+################# Labsheet Main #################
+
+################# Unit Test ################# Uncomment code inside of 'Unit Test' when unit testing
+################# Unit Test #################
