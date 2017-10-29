79d071c0e863b270ee09d9d1e9490256e811a262
James Lee <jamesl33info@gmail.com>
2017-10-29 15:47:31 +0000

    output is now numbered

--- Python/task5/part 1/main.py
+++ Python/task5/part 1/main.py
@@ -73,6 +73,6 @@ numberSet = find_largest_set(array) # gets the list of number sets in the matrix
 for i in range(len(numberSet)):
     if len(numberSet[i]) == 0:
         print("There are no sets of numbers in this matrix")
-    print("Number/Color = {0}\nSet = {1}\n".format(array[numberSet[i][0][0]][numberSet[i][0][1]], numberSet[i]))
+    print("{0}. Number/Color = {1}\n   Set = {2}\n".format(i + 1, array[numberSet[i][0][0]][numberSet[i][0][1]], numberSet[i]))
 
 ################# Labsheet Main #################
