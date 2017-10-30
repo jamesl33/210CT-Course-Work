1a92ac6c713081cfe5f45b3be252dfca9a662005
James Lee <jamesl33info@gmail.com>
2017-10-30 22:50:04 +0000

    uppdated unit testing

--- Python/task5/part 1/main.py
+++ Python/task5/part 1/main.py
@@ -82,6 +82,35 @@ for i in range(len(numberSet)):
 # import unittest
 
 # class UnitTest(unittest.TestCase):
+#     def test_check_neighbours(self):
+#         # left
+#         array = np.zeros((4, 4), int)
+#         array[1][1] = 1
+#         array[1][0] = 1
+
+#         self.assertEqual(check_neighbours(array, 1, 1), [(1, 0)])
+
+#         # right
+#         array = np.zeros((4, 4), int)
+#         array[1][1] = 1
+#         array[1][2] = 1
+
+#         self.assertEqual(check_neighbours(array, 1, 1), [(1, 2)])
+
+#         # above
+#         array = np.zeros((4, 4), int)
+#         array[1][1] = 1
+#         array[0][1] = 1
+
+#         self.assertEqual(check_neighbours(array, 1, 1), [(0, 1)])
+
+#         # below
+#         array = np.zeros((4, 4), int)
+#         array[1][1] = 1
+#         array[2][1] = 1
+
+#         self.assertEqual(check_neighbours(array, 1, 1), [(2, 1)])
+
 #     def test_check_if_set(self):
 #         array = np.zeros((4, 4), int)
 #         array[1][1] = 1
@@ -89,18 +118,26 @@ for i in range(len(numberSet)):
 #         array[2][1] = 1
 #         array[1][2] = 1
 #         array[1][0] = 1
-#         self.assertEqual(check_if_set(array, 1, 1), [(0, 1), (1, 1), (2, 1), (1, 0), (1, 2)])
 
-#     def test_set_finder(self):
+#         returnedValue = check_if_set(array, 1, 1)
+#         returnedValue.sort()
+
+#         correct = [(0, 1), (1, 1), (2, 1), (1, 0), (1, 2)]
+#         correct.sort()
+
+#         self.assertEqual(returnedValue, correct)
+
+#     def test_find_largest_set(self):
 #         array = np.zeros((4, 4), int)
+
 #         array[1][1] = 1
 #         array[0][1] = 1
 #         array[2][1] = 1
 #         array[1][2] = 1
 #         array[1][0] = 1
+
 #         self.assertEqual(find_largest_set(array), [[(0, 2), (0, 3), (1, 3), (2, 0), (2, 2), (2, 3), (3, 0), (3, 1), (3, 2), (3, 3)]])
 
-#     def test_set_finding(self):
 #         array = np.zeros((4, 4), int)
 
 #         for i in range(array.shape[0]):
