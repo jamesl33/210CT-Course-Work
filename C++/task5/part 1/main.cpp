9360f63a4ccc28ca8a247fd94651d86b3e224250
James Lee <jamesl33info@gmail.com>
2017-10-31 22:04:17 +0000

    finished task5 part1 in c++ (spaghetti code)

--- C++/task5/part 1/main.cpp
+++ C++/task5/part 1/main.cpp
@@ -60,92 +60,163 @@ std::vector<std::tuple<int, int>> check_if_set(std::vector<std::vector<int>>& ar
     return visited;
 }
 
-void print_array(std::vector<std::vector<int>>& array) {
-    for (auto i : array) {
-        for (auto j : i) {
-            std::cout << " " << j;
+std::vector<std::vector<std::tuple<int, int>>> get_all_sets(std::vector<std::vector<int>> array) {
+    std::vector<std::vector<std::tuple<int, int>>> allSets;
+    std::vector<std::tuple<int, int>> visited;
+    for (int i = 0; i < array.size(); i++) {
+        for (int j = 0; j < array[0].size(); j++) {
+            visited = {};
+            allSets.emplace_back(check_if_set(array, i, j, visited));
         }
-        std::cout << std::endl;
     }
+    return allSets;
+}
+
+std::vector<std::vector<std::tuple<int, int>>> find_largest_set(std::vector<std::vector<int>> array) {
+    std::vector<std::vector<std::tuple<int, int>>> sets;
+    int currentLargestSet = 0;
+    for (auto i : get_all_sets(array)) {
+        if (i.size() > currentLargestSet) {
+            sets = {};
+            std::sort (i.begin(), i.end());
+            auto it = std::find (sets.begin(), sets.end(), i);
+            if (it == sets.end()) {
+                sets.emplace_back(i);
+                currentLargestSet = i.size();
+            }
+        } else if (i.size() == currentLargestSet) {
+            std::sort (i.begin(), i.end());
+            auto it = std::find (sets.begin(), sets.end(), i);
+            if (it == sets.end()) {
+                sets.emplace_back(i);
+            }
+        }
+    }
+    return sets;
+}
+
+void pretty_print_array(std::vector<std::vector<int>>& array) {
+    for (int i = 0; i < array.size(); i++) {
+        std::cout << "[";
+        for (int j = 0; j < array[i].size(); j++) {
+            if (j == 0) {
+                std::cout << array[i][j];
+            } else {
+                std::cout << " " << array[i][j];
+            }
+        }
+        std::cout << "]" << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void pretty_print_find_largest_set_output(std::vector<std::vector<int>>& array, std::vector<std::vector<std::tuple<int, int>>>& numberSet) {
+        for (int i = 0; i < numberSet.size(); i++) {
+            std::cout << i + 1 << ". ";
+            std::cout << "Number/Color = " << array[std::get<0>(numberSet[i][0])][std::get<1>(numberSet[i][0])] << "\n   Set = [";
+            for (int j = 0; j < numberSet[i].size(); j++) {
+                if (j + 1 == numberSet[i].size()) {
+                    std::cout << "(" << std::get<0>(numberSet[i][j]) << ", " << std::get<1>(numberSet[i][j]) << ")";
+                } else {
+                    std::cout << "(" << std::get<0>(numberSet[i][j]) << ", " << std::get<1>(numberSet[i][j]) << "), ";
+                }
+            }
+            std::cout << "]" << std::endl << std::endl;
+        }
 }
 
 // ################# Labsheet main ################# // Comment out code inside of 'Labsheet Main' when running unit test
 
-// int main() {
-
-// }
+int main() {
+    std::vector<std::vector<int>> array = create_array(8, 8); // tested up to (10000, 10000) its takes a little while but does work
+    std::vector<std::vector<std::tuple<int, int>>> largestSet = find_largest_set(array);
+    pretty_print_array(array);
+    pretty_print_find_largest_set_output(array, largestSet);
+}
 
 // ################# Labsheet main #################
 
 // ################# Unit Test ################# // Uncomment code inside of 'Unit Test' when unit testing
 
-#define CATCH_CONFIG_MAIN
-#include "../../catch.hpp"
+// #define CATCH_CONFIG_MAIN
+// #include "../../catch.hpp"
 
-TEST_CASE("function to check neighbours of array 'x,y' position", "[check_neighbours]") {
-    std::vector<std::vector<int>> array;
+// TEST_CASE("function to check neighbours of array 'x,y' position", "[check_neighbours]") {
+//     std::vector<std::vector<int>> array;
 
-    SECTION("left") {
-        array = {{0, 0, 0, 0}, {1, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-        std::vector<std::tuple<int, int>> correct = {{1, 0}};
-        REQUIRE(check_neighbours(array, 1, 1) == correct);
-    }
+//     SECTION("left") {
+//         array = {{0, 0, 0, 0}, {1, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+//         std::vector<std::tuple<int, int>> correct = {{1, 0}};
+//         REQUIRE(check_neighbours(array, 1, 1) == correct);
+//     }
 
-    SECTION("right") {
-        array = {{0, 0, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-        std::vector<std::tuple<int, int>> correct = {{1, 2}};
-        REQUIRE(check_neighbours(array, 1, 1) == correct);
-    }
+//     SECTION("right") {
+//         array = {{0, 0, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+//         std::vector<std::tuple<int, int>> correct = {{1, 2}};
+//         REQUIRE(check_neighbours(array, 1, 1) == correct);
+//     }
 
-    SECTION("above") {
-        array = {{0, 1, 0, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-        std::vector<std::tuple<int, int>> correct = {{0, 1}};
-        REQUIRE(check_neighbours(array, 1, 1) == correct);
-    }
+//     SECTION("above") {
+//         array = {{0, 1, 0, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+//         std::vector<std::tuple<int, int>> correct = {{0, 1}};
+//         REQUIRE(check_neighbours(array, 1, 1) == correct);
+//     }
 
-    SECTION("below") {
-        array = {{0, 0, 0, 0}, {0, 1, 0, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}};
-        std::vector<std::tuple<int, int>> correct = {{2, 1}};
-        REQUIRE(check_neighbours(array, 1, 1) == correct);
-    }
-}
+//     SECTION("below") {
+//         array = {{0, 0, 0, 0}, {0, 1, 0, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}};
+//         std::vector<std::tuple<int, int>> correct = {{2, 1}};
+//         REQUIRE(check_neighbours(array, 1, 1) == correct);
+//     }
+// }
 
-TEST_CASE("function to check if there is a set of numbers in a matrix/array", "[check_if_set]") {
-    std::vector<std::vector<int>> array;
-    std::vector<std::tuple<int, int>> visited;
+// TEST_CASE("function to check if there is a set of numbers in a matrix/array", "[check_if_set]") {
+//     std::vector<std::vector<int>> array;
+//     std::vector<std::tuple<int, int>> visited;
 
-    SECTION("set of 5 numbers") {
-        array = {{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}};
-        visited = {};
+//     SECTION("set of 5 numbers") {
+//         array = {{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}};
+//         visited = {};
 
-        std::vector<std::tuple<int, int>> correct = {{ 0, 1 }, { 1, 0 }, { 1, 1 }, { 1, 2 }, { 2, 1 }};
-        std::vector<std::tuple<int, int>> returnedValue = check_if_set(array, 1, 1, visited);
-        std::sort (returnedValue.begin(), returnedValue.end());
+//         std::vector<std::tuple<int, int>> correct = {{ 0, 1 }, { 1, 0 }, { 1, 1 }, { 1, 2 }, { 2, 1 }};
+//         std::vector<std::tuple<int, int>> returnedValue = check_if_set(array, 1, 1, visited);
+//         std::sort (returnedValue.begin(), returnedValue.end());
 
-        REQUIRE(returnedValue == correct);
-    }
+//         REQUIRE(returnedValue == correct);
+//     }
 
-    SECTION("set of 2 numbers") {
-        array = {{0, 0, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-        visited = {};
+//     SECTION("set of 2 numbers") {
+//         array = {{0, 0, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+//         visited = {};
 
-        std::vector<std::tuple<int, int>> correct = {{1, 1}, {1, 2}};
-        std::vector<std::tuple<int, int>> returnedValue = check_if_set(array, 1, 1, visited);
-        std::sort (returnedValue.begin(), returnedValue.end());
+//         std::vector<std::tuple<int, int>> correct = {{1, 1}, {1, 2}};
+//         std::vector<std::tuple<int, int>> returnedValue = check_if_set(array, 1, 1, visited);
+//         std::sort (returnedValue.begin(), returnedValue.end());
 
-        REQUIRE(returnedValue == correct);
-    }
+//         REQUIRE(returnedValue == correct);
+//     }
 
-    SECTION("set of 16 numbers") {
-        array = {{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}};
-        visited = {};
+//     SECTION("set of 16 numbers") {
+//         array = {{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}};
+//         visited = {};
 
-        std::vector<std::tuple<int, int>> correct = {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 0}, {1, 1}, {1, 2}, {1, 3}, {2, 0}, {2, 1}, {2, 2}, {2, 3}, {3, 0}, {3, 1}, {3, 2}, {3, 3}};
-        std::vector<std::tuple<int, int>> returnedValue = check_if_set(array, 1, 1, visited);
-        std::sort (returnedValue.begin(), returnedValue.end());
+//         std::vector<std::tuple<int, int>> correct = {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 0}, {1, 1}, {1, 2}, {1, 3}, {2, 0}, {2, 1}, {2, 2}, {2, 3}, {3, 0}, {3, 1}, {3, 2}, {3, 3}};
+//         std::vector<std::tuple<int, int>> returnedValue = check_if_set(array, 1, 1, visited);
+//         std::sort (returnedValue.begin(), returnedValue.end());
 
-        REQUIRE(returnedValue == correct);
-    }
-}
+//         REQUIRE(returnedValue == correct);
+//     }
+// }
+
+// TEST_CASE("find the largest set of numbers in an array", "[find_largest_set]") {
+//     std::vector<std::vector<int>> array;
+//     std::vector<std::vector<std::tuple<int, int>>> correct;
+
+//     SECTION("set of 16 numbers") {
+//         array = {{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}};
+//         correct = {{{0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 0}, {1, 1}, {1, 2}, {1, 3}, {2, 0}, {2, 1}, {2, 2}, {2, 3}, {3, 0}, {3, 1}, {3, 2}, {3, 3}}};
+
+//         REQUIRE(find_largest_set(array) == correct);
+//     }
+// }
 
 // ################# Unit Test #################
