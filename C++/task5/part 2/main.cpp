d33a70063f8c0682ce60268d8d01a851afa2d273
James Lee <jamesl33info@gmail.com>
2017-11-02 14:58:29 +0000

    added input sanitization

--- C++/task5/part 2/main.cpp
+++ C++/task5/part 2/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include<limits>
 
 // Hoare partition scheme quick sort
 // https://en.wikipedia.org/wiki/Quicksort
@@ -39,7 +41,36 @@ void quick_sort(std::vector<int>& unsorted, int low, int high)
 // ################# Labsheet main ################# // Comment out code inside of 'Labsheet Main' when running unit test
 
 int main() {
+    srand(time(NULL));
+    std::vector<int> unsortedArray;
 
+    for (int i = 0; i < 10; i++) {
+        unsortedArray.emplace_back(rand() % 1000);
+    }
+
+    quick_sort(unsortedArray, 0, unsortedArray.size() - 1);
+
+    for (int i : unsortedArray) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Which element would you like to find? (0 indexed) ";
+    int input = 0;
+    while (true) {
+        if (!(std::cin >> input)) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Invalid input.  Try again: ";
+        } else if (input > unsortedArray.size() - 1) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Input out of range for array.  Try again: ";
+        } else {
+            std::cout << "The element in index " << input << " is " << unsortedArray[input] << std::endl;
+            break;
+        }
+    }
 }
 
 // ################# Labsheet main #################
