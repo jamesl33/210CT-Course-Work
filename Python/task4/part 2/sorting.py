82dec46a4078df079163c91abfa74541f6ba30df
James Lee <jamesl33info@gmail.com>
2017-11-17 10:20:53 +0000

    added docstring

--- Python/task4/part 2/sorting.py
+++ Python/task4/part 2/sorting.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 
 def quick_sort(array):
-    """ Use the quick sort algorithm to sort 'array'
-    input:
-        list: list of integers
-    output:
-        list: list of sorted integers created using the input list 'array'
-    """
+    """quick_sort: Use the quick sort algorithm to sort 'array'
 
+    :param array: List of integer, should work with strings
+    :output array: Sorted version of 'array'
+    """
     lessList, greaterList, equalList = [], [], []
     if len(array) > 1:
         pivot_point = array[-1]
-
         for item in array:
             if item == pivot_point:
-                equalList.append(item) # catch the item in the list which is the pivot point
+                equalList.append(item)
             elif item < pivot_point:
-                lessList.append(item) # create a list which has all elements less than the pivot point in
+                lessList.append(item)
             else:
-                greaterList.append(item) # create a list which has all elements greater than the pivot point in
-
-        return quick_sort(lessList) + equalList + quick_sort(greaterList) # recursive call using the three lists created
+                greaterList.append(item)
+        return quick_sort(lessList) + equalList + quick_sort(greaterList)
     else:
         return array
