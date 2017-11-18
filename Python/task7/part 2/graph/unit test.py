e7fb3a9965ffe531c591bb31f12abb9331561c08
James Lee <jamesl33info@gmail.com>
2017-11-18 14:20:11 +0000

    added indegrees function and topological sorting function

--- Python/task7/part 2/graph/unit test.py
+++ Python/task7/part 2/graph/unit test.py
@@ -13,7 +13,7 @@ class UnitTest(unittest.TestCase):
         g.add_node(n3)
 
         self.assertEqual(g.vertices, set({1, 2, 3}))
-        self.assertEqual(g.edges, {1: set({2, 3}), 2: set({1, 3}), 3: set({1, 2})})
+        self.assertEqual(g.edges, {1: set({2}), 2: set({1}), 3: set({1, 2})})
         self.assertEqual(g.weights, {(1, 2): 2, (2, 1): 2, (3, 1): 2, (3, 2): 4})
 
     def test_remove(self):
@@ -25,6 +25,39 @@ class UnitTest(unittest.TestCase):
         self.assertEqual(g.vertices, set({2}))
         self.assertEqual(g.edges, {2: set({})})
         self.assertEqual(g.weights, {})
+
+    def test_get_indegrees(self):
+        n0 = weighted_node(0, [], {})
+        n1 = weighted_node(1, [], {})
+        n2 = weighted_node(2, [3], {(2, 3): 1})
+        n3 = weighted_node(3, [1], {(3, 1): 1})
+        n4 = weighted_node(4, [0, 1], {(4, 0): 1, (4, 1): 1})
+        n5 = weighted_node(5, [0, 2], {(5, 0): 1, (5, 2): 1})
+        g = weighted_graph([n0, n1, n2, n3, n4, n5])
+
+        self.assertEqual(g.vertices, set({0, 1, 2, 3, 4, 5}))
+        self.assertEqual(g.edges, {2: set({3}), 3: set({1}), 4: set({0, 1}), 5: set({0, 2})})
+        self.assertEqual(g.weights, {(2, 3): 1, (3, 1): 1, (4, 0): 1, (4, 1): 1, (5, 0): 1, (5, 2): 1})
+        self.assertEqual(g._get_indegrees(), {0: 2, 1: 2, 2: 1, 3: 1, 4: 0, 5: 0})
+
+    def test_topological_sort(self):
+        n0 = weighted_node(5, [11], {(5, 11): 1})
+        n1 = weighted_node(11, [2, 9, 10], {(11, 2): 1, (11, 9): 1, (11, 10): 1})
+        n2 = weighted_node(2, [], {})
+        n3 = weighted_node(7, [8, 11], {(7, 8): 1, (7, 11): 1})
+        n4 = weighted_node(8, [9], {(8, 9): 1})
+        n5 = weighted_node(9, [], {})
+        n6 = weighted_node(3, [8, 10], {(3, 8): 1, (3, 10): 1})
+        n7 = weighted_node(10, [], {})
+        g = weighted_graph([n0, n1, n2, n3, n4, n5, n6, n7])
+        self.assertEqual(g.topological_sort(), [7, 5, 11, 3, 10, 8, 9, 2])
+
+    def test_topological_sort_error(self):
+        n0 = weighted_node(1, [2], {(1, 2): 1})
+        n1 = weighted_node(2, [1], {(2, 1): 1})
+        g = weighted_graph([n0, n1])
+        with self.assertRaises(TypeError):
+            g.topological_sort()
 
 if __name__ == '__main__':
     unittest.main()
--- Python/task7/part 2/graph/weighted_graph.py
+++ Python/task7/part 2/graph/weighted_graph.py
@@ -15,6 +15,10 @@ class weighted_graph(graph):
         graph.__init__(self, nodes)
 
     def add_node(self, node):
+        """add_node
+
+        :param node: Overridden 'add_node' function to include adding of 'weights'
+        """
         assert(isinstance(node, weighted_node))
         self._add_vertex(node.value)
         self._add_weights(node.weights)
@@ -22,10 +26,61 @@ class weighted_graph(graph):
             self._add_edge(node.value, connection)
 
     def remove_node(self, node):
+        """remove_node
+
+        :param node: Overridden 'remove_node' function to include removal of 'weights'
+        """
         assert(isinstance(node, weighted_node))
         self._remove_vertex(node.value)
         self._remove_edge(node.value)
         self._remove_weights(node.weights)
+
+    def topological_sort(self):
+        """topological_sort: Function to topologically sort a graph
+        https://en.wikipedia.org/wiki/Topological_sorting
+
+        :returns: List representing the graph nodes in topologically sorted order
+        """
+        stack = []
+        visited = set()
+        topologicalOrder = []
+
+        def _visit(node):
+            if node in visited:
+                return
+            if node in stack:
+                raise TypeError('Graph contains a cycle')
+            stack.append(node)
+            if node in self.edges:
+                for neighbour in self.edges[node]:
+                    _visit(neighbour)
+            stack.pop()
+            topologicalOrder.insert(0, node)
+            visited.add(node)
+
+        while visited != self.vertices:
+            for node in self.vertices:
+                _visit(node)
+        return topologicalOrder
+
+    def _get_indegrees(self):
+        """_get_indegrees: Function which returns indegrees of all vertices
+
+        :return dict
+        """
+        indegrees = {}
+        for n in self.vertices:
+            indegrees[n] = 0
+        for vertex in self.vertices:
+            if vertex in self.edges:
+                for neighbour in self.edges[vertex]:
+                    indegrees[neighbour] += 1
+        return indegrees
+
+    def _add_edge(self, vertexA, vertexB):
+        if vertexA not in self.edges:
+            self.edges[vertexA] = set()
+        self.edges[vertexA].add(vertexB)
 
     def _add_weights(self, weights):
         for weight in weights:
